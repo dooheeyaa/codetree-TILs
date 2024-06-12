a7f5d8caa0e9d2278fcd0b00353ff728dd82ce86
doohee <rkdengml0920@naver.com>
2024-06-12 12:13:15 +0900

    Title: 점수 입출력, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240612/점수 입출력/enter-int-and-print-score.js
+++ 240612/점수 입출력/enter-int-and-print-score.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-let input = fs.readFileSync(0).toString();
+let n = fs.readFileSync(0).toString();
 
-console.log(`Your score is ${input} point.`);
+console.log(`Your score is ${n} point.`);
