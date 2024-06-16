4b601ac1ea232ff124d0e46ae0a61fedc9f19e5c
doohee <rkdengml0920@naver.com>
2024-06-16 13:07:19 +0900

    Title: 시험 통과 여부 확인하기, Time: 223ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240616/시험 통과 여부 확인하기/verify-test-passed.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let score = fs.readFileSync(0).toString().trim();
+score = Number(score);
+
+if (score >= 80 ){
+    console.log('pass');
+} else {
+    console.log(`${80 - score} more score`);
+}
