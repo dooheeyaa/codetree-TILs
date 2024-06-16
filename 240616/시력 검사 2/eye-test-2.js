9238677523e6e55d18b69cbe9957a59cf23ac3b4
doohee <rkdengml0920@naver.com>
2024-06-16 13:43:49 +0900

    Title: 시력 검사 2, Time: 176ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240616/시력 검사 2/eye-test-2.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let a = fs.readFileSync(0).toString().trim();
+a = Number(a);
+
+if(a >= 1.0){
+    console.log('High');
+}else if (a >= 0.5){
+    console.log('Middle');
+}else {
+    console.log('Low');
+}
