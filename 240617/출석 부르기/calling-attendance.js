71a4324b000fc3286064fc8237db207aff6e193d
doohee <rkdengml0920@naver.com>
2024-06-17 10:54:05 +0900

    Title: 출석 부르기, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240617/출석 부르기/calling-attendance.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+let number = fs.readFileSync(0).toString().trim();
+number = Number(number);
+
+if(number === 1){
+    console.log('John');
+}else if(number === 2){
+    console.log('Tom');
+}else if(number === 3){
+    console.log('Paul');
+}else {
+    console.log('Vacancy');
+}
