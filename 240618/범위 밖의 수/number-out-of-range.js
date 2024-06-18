95c1d41c9927ecbda3d0e4285424ba3ab4c80152
doohee <rkdengml0920@naver.com>
2024-06-18 23:00:30 +0900

    Title: 범위 밖의 수, Time: 203ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240618/범위 밖의 수/number-out-of-range.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim();
+let a = Number(input);
+
+if(a < 10 || a > 20){
+    console.log('yes');
+}else {
+    console.log('no');
+}
