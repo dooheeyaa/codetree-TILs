f0e6dfded25cd01b7edc40910dd760cecdc64532
doohee <rkdengml0920@naver.com>
2024-06-18 23:39:46 +0900

    Title: 좀 더 어려운 수학 점수, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240618/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split('\n');
+let a = input[0].split(' ');
+let b = input[1].split(' ');
+
+let aMath = Number(a[0]);
+let aEng = Number(a[1]);
+
+let bMath = Number(b[0]);
+let bEng = Number(b[1]);
+
+if(aMath > bMath){
+    console.log('A')
+}else if(aMath < bMath){
+    console.log('B');
+}else if(aMath === bMath && aEng > bEng){
+    console.log('A');
+}else if(aMath === bMath && aEng < bEng){
+    console.log('B');
+}
