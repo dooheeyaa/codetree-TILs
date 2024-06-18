7d3f066ec66389ce0389fd6cb0df4319115d43db
doohee <rkdengml0920@naver.com>
2024-06-18 23:15:50 +0900

    Title: 굉장한 숫자, Time: 239ms, Memory: 14MB, Status: Passed - CodeTree

--- 240618/굉장한 숫자/amazing-number.js
+++ 240618/굉장한 숫자/amazing-number.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let n = Number(fs.readFileSync(0).toString().trim());
 
-if((n % 2 !== 0 && n % 3 === 0) || (a % 2 === 0 && a % 5 === 0)){
+if((n % 2 !== 0 && n % 3 === 0) || (n % 2 === 0 && n % 5 === 0)){
     console.log('true');
 }else {
     console.log('false');
