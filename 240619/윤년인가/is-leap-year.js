79e10fa04286a767b5d21b49193f7d0fc6e8c17d
doohee <rkdengml0920@naver.com>
2024-06-19 11:24:35 +0900

    Title: 윤년인가, Time: 216ms, Memory: 14MB, Status: Passed - CodeTree

--- 240619/윤년인가/is-leap-year.js
+++ 240619/윤년인가/is-leap-year.js
@@ -2,8 +2,12 @@ const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim();
 let y = Number(input);
 
-if ((y % 4 === 0) || (y % 1000 === 0 && y % 400 !== 0)){
-    console.log('true');
-}else{
+if (y % 100 === 0 && y % 400 !== 0){
     console.log('false');
+}else{
+    if(y % 4 === 0){
+        console.log('true');
+    }else{
+        console.log('false');
+    }
 }
