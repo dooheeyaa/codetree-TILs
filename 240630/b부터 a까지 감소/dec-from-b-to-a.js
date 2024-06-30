27877668fe70826587f198e0a134fe472f83609c
doohee <rkdengml0920@naver.com>
2024-06-30 13:11:27 +0900

    Title: b부터 a까지 감소, Time: 171ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240630/b부터 a까지 감소/dec-from-b-to-a.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let result = "";
+for(i=b; i >= a; i--){
+    result = result + i + " ";
+}
+console.log(result);
