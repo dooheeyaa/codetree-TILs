d8a12ab2bbb55e419b7b34d1e58a3bcff3685f8e
doohee <rkdengml0920@naver.com>
2024-07-02 12:06:32 +0900

    Title: n번 더하기, Time: 199ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240702/n번 더하기/add-n-times.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+let a = Number(input[0]);
+let n = Number(input[1]);
+let result = a;
+
+for(i = 0; i < n; i++){
+    result += n;
+    console.log(result);
+}
