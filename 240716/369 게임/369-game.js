65a56abd34f86dc5198ab9cc7e51d9fd000f4f46
doohee <rkdengml0920@naver.com>
2024-07-16 11:34:53 +0900

    Title: 369 게임, Time: 164ms, Memory: 15MB, Status: Passed - CodeTree

--- 240716/369 게임/369-game.js
+++ 240716/369 게임/369-game.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim();
 let n = Number(input);
-let result = '';
-
-for(let i = 1; i <= n; i++){
-    if(i % 3 === 0 || i % 10 === 3 || i % 10 === 6 || i % 10 === 9 ){
-        result += 0 + ' ';
-    }else{
-        result += i + ' ';
+let result = "";
+for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0) {
+        result += "0 ";
+    }
+    else if (i % 10 === 3 || i % 10 === 6 || i % 10 === 9) {
+        result += "0 ";
+    }
+    else if (parseInt(i / 10) === 3 || parseInt(i / 10) === 6 || parseInt(i / 10) === 9) {
+        result += "0 ";
+    }
+    else {
+        result += i + " ";
     }
 }
+
 console.log(result);
