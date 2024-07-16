867ebc1aa5966fc7bb787e4b262cae8a5620f02b
doohee <rkdengml0920@naver.com>
2024-07-16 10:29:07 +0900

    Title: 2 또는 3의 배수, Time: 223ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240716/2 또는 3의 배수/multiple-of-2-or-3.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim();
+let n = Number(input);
+let result = "";
+
+for(let i = 1; i <= n; i++){
+    if(i % 2 === 0 || i % 3 === 0){
+        result += 1 + " ";
+    }else{
+        result += 0 + " ";
+    }
+}
+
+console.log(result);
