e169b6c1f91c36918d843220b525494f08142752
doohee <rkdengml0920@naver.com>
2024-07-18 09:57:11 +0900

    Title: 특정 조건의 온전수 구하기, Time: 225ms, Memory: 14MB, Status: Passed - CodeTree

--- 240718/특정 조건의 온전수 구하기/print-onjeonsu.js
+++ 240718/특정 조건의 온전수 구하기/print-onjeonsu.js
@@ -3,9 +3,10 @@ let input = fs.readFileSync(0).toString().trim();
 let n = Number(input);
 let result = '';
 
-for(let i = 1; 1 <= n; i++){
-    if(i % 2 !== 0 && i % 10 !== 0 && i % 3 !== 0 && i % 9 === 0){
-        result += i + ' ';
+for(let i = 1; i <= n; i++){
+    if(i % 2 === 0 || i % 10 === 5 || (i % 3 === 0 && i % 9 !== 0)){
+        continue;
     }
+    result += i + ' ';
 }
 console.log(result);
