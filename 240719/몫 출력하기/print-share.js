9d54195c5d11612c009d83d9055e029d996bdfa6
doohee <rkdengml0920@naver.com>
2024-07-19 13:42:13 +0900

    Title: 몫 출력하기, Time: 5007ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

--- 240719/몫 출력하기/print-share.js
+++ 240719/몫 출력하기/print-share.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim().split('\n');
 let index = 0;
-let n = 0;
+let count = 0;
 
-while(n <= 3){
+while(count <= 3){
     let number = Number(input[index]);
-    n++;
-    index++;
+    index ++;
 
     if(number % 2 === 0){
         console.log(Math.floor(number / 2));
+        count ++;
     }
 }
